6d6e1f7cba6621b6dbc943f8f50770f4e9b7ff0a
BHessel <hesselfitness@gmail.com>
2020-10-14 09:01:21 -0400

    final

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require 'pry'
+
+class Student < User
+
+attr_reader :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(knowledge)
+        #binding.pry
+        @knowledge << knowledge
+    end
 
 end
